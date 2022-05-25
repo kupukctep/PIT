dc4d422f661a31f01b556d71226b8b9a3082a670
Justyna <justyna.michalik93@gmail.com>
2022-05-25 15:32:58 +0300

    Add style for landing page

--- PIT/src/App.svelte
+++ PIT/src/App.svelte
@@ -2,13 +2,13 @@
     import Balance from "./component/Balance.svelte";
     import Intro from "./component/Intro.svelte";
     import Trade from "./component/Trade.svelte";
-    import TransactionLog from "./component/TransactionLog.svelte";
 </script>
 
-<main>
+<main class="min-h-screen bg-gray-100">
     <section class="relative flex items-center justify-between flex-col h-screen lg:overflow-hidden">
+    <div class="bg-white shadow sm:rounded-lg mt-8">
         <div class="relative z-20 w-full">
-            <div class="w-full md:w-3/4 mx-auto py-6 sm:px-0 lg:px-8">
+            <div class="w-full mx-auto py-6 sm:px-0 lg:px-8">
                 <div class="sm:mx-auto sm:w-full sm:max-w-3xl">
                     <div class="px-4 sm:px-6">
                         <Intro/>
@@ -18,6 +18,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </section>
 </main>
 

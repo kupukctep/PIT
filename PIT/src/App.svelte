92dfc7677a497e801e5e26b027f506e95b8ce4f2
Justyna <justyna.michalik93@gmail.com>
2022-05-25 13:40:20 +0300

    Add tailwind css

--- PIT/src/App.svelte
+++ PIT/src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Balance from "./component/Balance.svelte";
 
-	export let name;
 </script>
 
 <main>
@@ -14,13 +13,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {

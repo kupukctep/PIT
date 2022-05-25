f8830b43c37b7729c6ef2af97a9e550f7c1bfcd4
Justyna <justyna.michalik93@gmail.com>
2022-05-25 15:13:37 +0300

    Add styles for table

--- PIT/src/component/Assets.svelte
+++ PIT/src/component/Assets.svelte
@@ -3,20 +3,31 @@
         {ticker: 'LYFT', quantity: 23, value: 15000},
         {ticker: 'WEN', quantity: 100, value: 50000}]
 </script >
-<table>
-    <thead>
+
+<table class="min-w-full divide-y divide-gray-200">
+    <thead class="bg-white">
     <tr>
-        <th>Ticker</th>
-        <th>Quantity</th>
-        <th>Value</th>
+        <th scope="col"
+            class="font-bold px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            TICKER
+        </th>
+        <th scope="col"
+            class="font-bold px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            QUANTITY
+        </th>
+
+        <th scope="col"
+            class="font-bold px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+            VALUE
+        </th>
     </tr>
     </thead>
-    <tbody>
+    <tbody class="bg-white divide-y divide-gray-200">
     {#each assets as asset}
         <tr>
-            <td>{asset.ticker}</td>
-            <td>{asset.quantity}</td>
-            <td>{asset.value}</td>
+            <td class="px-6 py-2">{asset.ticker}</td>
+            <td class="px-6 py-2">{asset.quantity}</td>
+            <td class="px-6 py-2 text-right">{asset.value}</td>
         </tr>
     {/each}
     </tbody>

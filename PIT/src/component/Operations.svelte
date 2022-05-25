9bb04a5afa93e9747bc3616c580996db2ff6081a
Ilja Tolliu, Kirill Stepanov <ilja@codeborne.com, kirill.stepanov@codeborne.com>
2022-05-25 14:19:47 +0300

    rename to assets

--- PIT/src/component/Operations.svelte
+++ PIT/src/component/Operations.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    let operations = [{ticker: 'APPL', quantity: 100, value : 21000},
+    let assets = [{ticker: 'APPL', quantity: 100, value : 21000},
         {ticker: 'LYFT', quantity: 23, value: 15000},
         {ticker: 'WEN', quantity: 100, value: 50000}]
 </script >
@@ -12,11 +12,11 @@
     </tr>
     </thead>
     <tbody>
-    {#each operations as operation}
+    {#each assets as asset}
         <tr>
-            <td>{operation.ticker}</td>
-            <td>{operation.quantity}</td>
-            <td>{operation.value}</td>
+            <td>{asset.ticker}</td>
+            <td>{asset.quantity}</td>
+            <td>{asset.value}</td>
         </tr>
     {/each}
     </tbody>
